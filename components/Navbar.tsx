6f30ee1ca6db9184c88fa22fe2ab544314364930
Karmanya20 <99268305+Karmanya20@users.noreply.github.com>
2024-09-20 09:15:28 +0530

    Close button to Hamburger menu added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,6 +49,13 @@ export default function Navbar() {
               </svg>
             </Button>
             <ul className={`lg:hidden fixed top-0 left-0 z-50 w-full h-full bg-gray-900 bg-opacity-90 text-white flex flex-col items-center justify-center space-y-8 p-4 transition-all duration-300 ${isOpen ? 'block' : 'hidden'}`}>
+            <button
+          onClick={toggleMenu} 
+          className="absolute top-4 right-4 text-white text-3xl focus:outline-none"
+        >
+          &#10005; {/* X (Close Icon) */}
+        </button>
+
         <li><Link href="/" className="text-white px-3 py-2 rounded-md text-sm font-medium">
                 Introduction
               </Link></li>
