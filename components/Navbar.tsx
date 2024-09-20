ef87366af843074cef365607fda38092ae5eb908
Karmanya20 <99268305+Karmanya20@users.noreply.github.com>
2024-09-20 08:57:37 +0530

    Hamburger menu added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,14 @@
+"use client";
 import Link from 'next/link'
 import { Button } from "@/components/ui/button"
+import { useState } from 'react'
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="border-b">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -30,8 +37,10 @@ export default function Navbar() {
           </div>
           <div className="md:hidden">
             <Button
+            id='menuButton'
               variant="ghost"
               size="icon"
+              onClick={toggleMenu}
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             >
               <span className="sr-only">Open main menu</span>
@@ -39,6 +48,23 @@ export default function Navbar() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </Button>
+            <ul className={`lg:hidden flex flex-col bg-gray-700 text-white space-y-4 p-10 transition-all duration-300 ${isOpen ? 'block' : 'hidden'}`}>
+        <li><Link href="/" className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                Introduction
+              </Link></li>
+        <li><Link href="/video-demonstration" className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                Video Demo
+              </Link></li>
+        <li><Link href="/prototype" className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                Prototype
+              </Link></li>
+        <li><Link href="/resources" className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                Resources
+              </Link></li>
+              <li> <Link href="/team" className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                Team
+              </Link></li>
+      </ul>
           </div>
         </div>
       </div>
